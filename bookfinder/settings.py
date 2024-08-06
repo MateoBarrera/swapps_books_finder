db64a362149f078d9ee56c3c581357a97b071568
MateoBarrera <materile1997@gmail.com>
2024-08-06 01:18:27 -0500

    build: remove database config

--- bookfinder/settings.py
+++ bookfinder/settings.py
@@ -81,12 +81,13 @@ WSGI_APPLICATION = 'bookfinder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+DATABASES = {}
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} """
 
 
 # Password validation
--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-# Salir inmediatamente si un comando falla
-set -e
-
-
 # build_files.sh
 pip install -r requirements.txt
-pip install pysqlite3
 
 python3 manage.py collectstatic --no-input --clear
